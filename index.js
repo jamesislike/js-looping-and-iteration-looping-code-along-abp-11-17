c330075b2c92e27a3d48c4607a32ef2776ecf465
James Gomis <jamesislike@gmail.com>
2017-11-12 19:28:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+for printBadges(arrayNames){
+  arrayNames  =["james","john","jean"]
+
+  for(let i = 0 ; i < arrayNames.length; i++){
+
+    console.log(`welcome ${arrayNames[i]}! You are employee #${i+1}.`)
+  }
+  return arrayNames
+}
+
+
+function coinToss() {
+  return Math.Random() >= 0.5;
+}
+
+function tailsNeverFails() {
+  let counter = 0
+  
+}
